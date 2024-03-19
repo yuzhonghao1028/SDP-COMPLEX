1caa94400333e2a23581cbbbba87823a3d60d3c2
yuzhonghao1028 <162937942+yuzhonghao1028@users.noreply.github.com>
2024-03-19 15:37:54 +0800

    Update 3.basic processing.R

--- snRNA-seq_injury/3.basic processing.R
+++ snRNA-seq_injury/3.basic processing.R
@@ -4,27 +4,27 @@ library(ggplot2)
 library(reshape2)
 
 # load RDS data
-seurat.obj.combined <- readRDS(file = "results/SCT.CCA/undefined verify/goat.ON.snRNA_SCT.CCA_undefined verify.rds")
+seurat.obj.combined <- readRDS(file = "snRNA-seq_injury/results/SCT.CCA/undefined verify/goat.ON.snRNA_SCT.CCA_undefined verify.rds")
 
 # dimplot for celltype
 seurat.col <- pal_locuszoom("default", alpha = 1)(7)
 seurat.obj.combined$celltype <- factor(seurat.obj.combined$celltype,levels = c("Astrocyte","MP","OPC","Fibroblast","Oligodendrocyte","VEC","Vascular Mural Cell"))
 DimPlot(seurat.obj.combined,group.by = "celltype",split.by = "group",cols = seurat.col)+NoAxes()+labs(title = "")
-ggsave("results/SCT.CCA/basic processing/celltype.splitby.group.umap.pdf",width = 12,height = 5)
+ggsave("snRNA-seq_injury/results/SCT.CCA/basic processing/celltype.splitby.group.umap.pdf",width = 12,height = 5)
 
 DimPlot(seurat.obj.combined,group.by = "celltype",cols = seurat.col)+NoAxes()+labs(title = "")
 ggsave("result/SCT.CCA/basic processing/celltype.umap.pdf",width = 7.5,height = 5)
 DimPlot(seurat.obj.combined,group.by = "celltype",split.by = "group",cols = seurat.col)+NoAxes()+labs(title = "")
-ggsave("results/SCT.CCA/basic processing/celltype.splitby.group.umap.pdf",width = 12,height = 5)
+ggsave("snRNA-seq_injury/results/SCT.CCA/basic processing/celltype.splitby.group.umap.pdf",width = 12,height = 5)
 
 # findallmarkers
 DEGs <- FindAllMarkers(seurat.obj.combined,logfc.threshold = 0.25,min.pct = 0.25,verbose = T)
-write.table(deg1,file = "results/SCT.CCA/basic processing/celltype_deg.xls",sep = "\t")
+write.table(deg1,file = "snRNA-seq_injury/results/SCT.CCA/basic processing/celltype_deg.xls",sep = "\t")
 
 # dotplot for marker genes
 DotPlot(seurat.obj.combined,features = c("MOBP","MBP","GFAP","ALDH1L1","FN1","COL3A1","LHFPL3","PCDH15",
                                       "ERG","FLT1","P2RY12","CD36","CALD1","ACTA2"), cols = c("grey", "#ff70b5"))+RotatedAxis()+ggplot2:::coord_flip()+ylab("")+xlab("")
-ggsave(filename = "results/SCT.CCA/basic processing/celltype_marker_dotplot.pdf",width = 6,height = 5.5)
+ggsave(filename = "snRNA-seq_injury/results/SCT.CCA/basic processing/celltype_marker_dotplot.pdf",width = 6,height = 5.5)
 
 # celltype propotion stackbarplot
 data <- as.data.frame(table(seurat.obj.combined$celltype,seurat.obj.combined$group))
@@ -55,7 +55,7 @@ ggplot(data, aes( x = Var2, y=percentage,fill = Var1))+
   scale_y_continuous(labels = scales::percent_format(scale = 100))+
   geom_text(aes(label=label),size=4,color="black",position = "stack")
 
-ggsave(filename = "results/SCT.CCA/basic processing/stack barplot.pdf",width = 4.5,height = 5)
+ggsave(filename = "snRNA-seq_injury/results/SCT.CCA/basic processing/stack barplot.pdf",width = 4.5,height = 5)
 
 # DEGs two-way bar charts
 df <- read.csv(file = "DEG number_log2fc1.csv")
@@ -79,4 +79,4 @@ scale_y_continuous(breaks = seq(-900, 900, 100))+
                              "dodgerblue"))+
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
   
-ggsave(filename = "results/SCT.CCA/basic processing/Two-way bar charts_log2fc1.pdf",width = 4,height = 5)
+ggsave(filename = "snRNA-seq_injury/results/SCT.CCA/basic processing/Two-way bar charts_log2fc1.pdf",width = 4,height = 5)
